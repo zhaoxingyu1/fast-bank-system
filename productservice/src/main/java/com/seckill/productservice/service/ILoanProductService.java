c0a0d95901d376eb9946f784ec132314e089e6c1
七画一只妖 <1157529280@qq.com>
2022-02-13 18:12:59 +0800

    粗略完成LoanProduct的service部分

--- productservice/src/main/java/com/seckill/productservice/service/ILoanProductService.java
+++ productservice/src/main/java/com/seckill/productservice/service/ILoanProductService.java
@@ -3,9 +3,25 @@ package com.seckill.productservice.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.seckill.productservice.entity.LoanProductEntity;
 
+import java.util.List;
+
 /**
  * @Author: 七画一只妖
  * @Date: 2022/2/13 16:33
  */
 public interface ILoanProductService{
+    //新增贷款产品
+    boolean addLoanProduct(LoanProductEntity loanProductEntity);
+
+    //删除贷款产品
+    boolean deleteLoanProduct(int loanProductId);
+
+    //更新贷款产品
+    boolean updateLoanProduct(LoanProductEntity loanProductEntity);
+
+    //查找贷款产品（根据ID）
+    LoanProductEntity findLoanProductById(int loanProductId);
+
+    //查找贷款产品（所有）
+    List<LoanProductEntity> findAll();
 }
