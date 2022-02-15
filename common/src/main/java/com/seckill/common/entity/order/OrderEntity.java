4f7bcacadecb3f4099585e1c52d2eddb47617a60
ChenZheng <1007324849@qq.com>
2022-02-15 20:25:53 +0800

    fix

--- common/src/main/java/com/seckill/common/entity/order/OrderEntity.java
+++ common/src/main/java/com/seckill/common/entity/order/OrderEntity.java
@@ -1,25 +1,22 @@
 package com.seckill.common.entity.order;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
 import com.seckill.common.entity.product.BaseProduct;
+import lombok.Data;
 
 /**
  * @author : 陈征
  * @date : 2022-02-14 20:29
  */
 
+@Data
+@TableName("order")
 public class OrderEntity {
     @TableId(type = IdType.ASSIGN_UUID)
-    @TableField("order_id")
     String orderId;
 
-    @TableField("user_id")
     String userId;
 
-    @TableField("product_id")
     String productId;
 
     @TableField(fill = FieldFill.INSERT)
