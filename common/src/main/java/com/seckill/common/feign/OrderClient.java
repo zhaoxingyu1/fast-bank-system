578903d2b8409a9b28333d89d72ef57c0e748c2d
ChenZheng <1007324849@qq.com>
2022-02-17 18:43:05 +0800

    refactor: 统一配置类

--- common/src/main/java/com/seckill/common/feign/OrderClient.java
+++ common/src/main/java/com/seckill/common/feign/OrderClient.java
@@ -1,4 +1,4 @@
-package com.seckill.common.feign.clients;
+package com.seckill.common.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
